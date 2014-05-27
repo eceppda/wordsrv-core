2c52eabbf80a8ab2c7137366d6bf62416ed1bae4
Jeff Hemminger <jeff@kropek.org>
2014-05-27 18:49:45 +1000

    Initial commit with storm dependency added

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject wordsrv-core "0.1.0-SNAPSHOT"
+  :description "the word is served"
+  :url "http://pp-femurchasm.gnu-darwin.org"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.apache.storm/storm-core "0.9.1-incubating"]])

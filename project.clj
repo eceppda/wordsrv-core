cd6f65d67c3b10dd34261076be349f0c02e09b28
Jeff Hemminger <jeff@kropek.org>
2014-05-27 21:39:24 +1000

    added some storm-related code, and it still compiles...

--- project.clj
+++ project.clj
@@ -3,5 +3,17 @@
   :url "http://pp-femurchasm.gnu-darwin.org"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.apache.storm/storm-core "0.9.1-incubating"]])
+  :aot :all
+;  :dependencies [[org.clojure/clojure "1.5.1"]
+                                        ;                 [org.apache.storm/storm-core "0.9.1-incubating"]]
+  :dependencies [[commons-collections/commons-collections "3.2.1"]]
+  :profiles {:dev
+              {:dependencies [[org.apache.storm/storm-core "0.9.1-incubating"]
+                              [org.clojure/clojure "1.5.1"]
+                              [org.testng/testng "6.8.8"]
+                              [org.easytesting/fest-assert "1.4"]
+                              [org.mockito/mockito-all "1.9.5"]
+                              [org.jmock/jmock "2.6.0"]]}}
+  :min-lein-version "2.3.4"
+  )
+
